53ca63bb6ee88983be6d66a2db053272190bffb1
Samuele Dassatti <samueledassatti99@gmail.com>
2017-12-30 17:32:11 +0100

    renaming stuff

--- src/Main.js
+++ src/Main.js
@@ -1,38 +1,38 @@
-import React, {Component} from "react"
-import App from "./App"
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
 
 class Main extends Component {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div className="main">
-                <div className="sidebarHeader" />
-                <div className="sidebar">
-                    <App />
-                </div>
-                <div className="mainBodyHeader" />
-                <div className="mainBody">
-                    <div className="large" />
-                    <div />
-                    <div className="tall" />
-                    <div className="tall" />
-                    <div />
-                    <div className="wide" />
-                    <div className="tall" />
-                    <div className="wide" />
-                    <div />
-                    <div />
-                    <div />
-                    <div />
-                    <div />
-                    <div />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="main">
+        <div className="sidebarHeader" />
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="mainBodyHeader" />
+        <div className="mainBody">
+          <div className="large" />
+          <div />
+          <div className="tall" />
+          <div className="tall" />
+          <div />
+          <div className="wide" />
+          <div className="tall" />
+          <div className="wide" />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Main
+export default Main;
