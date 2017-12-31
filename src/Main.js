eeac9920284e8f78f93d30bd358e42b80472974a
Samuele Dassatti <samueledassatti99@gmail.com>
2017-12-31 13:27:10 +0100

    Added material icons (not yet implemented) and new SVG logo

--- src/Main.js
+++ src/Main.js
@@ -1,78 +1,76 @@
-import React, {Component} from "react"
-import Sidebar from "./Sidebar"
-import SidebarHeader from "./SidebarHeader"
-import Tile from "./Tile"
-import AppBar from "material-ui/AppBar"
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
-import FlatButton from "material-ui/FlatButton"
-import "./App.css"
-import "./Tiles.css"
+import React, { Component } from "react";
+import Sidebar from "./Sidebar";
+import SidebarHeader from "./SidebarHeader";
+import Tile from "./Tile";
+import AppBar from "material-ui/AppBar";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import FlatButton from "material-ui/FlatButton";
+import "./App.css";
+import "./Tiles.css";
 
 class Main extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            showHidden: false,
-        }
-    }
+    this.state = {
+      showHidden: false
+    };
+  }
 
-    render() {
-        const visibleItems = [
-            <Tile value={true} hidden={false} title="Test" className="large" />,
-            <Tile value={true} hidden={false} title="ABC" />,
-            <Tile value={false} hidden={false} title="Ehi" className="tall" />,
-            <Tile value={false} hidden={false} title="Dumb" className="wide" />,
-            <Tile value={true} hidden={false} title="Clone" className="tall" />,
-            <Tile value={false} hidden={false} title="Tall" className="tall" />,
-            <Tile value={true} hidden={false} title="Wide" className="wide" />,
-            <Tile value={true} hidden={false} title="Small" />,
-            <Tile value={false} hidden={false} title="Still readin?" />,
-            <Tile value={true} hidden={false} title="Man!!" />,
-            <Tile value={false} hidden={false} title="Stop" />,
-            <Tile value={false} hidden={false} title="This is" />,
-            <Tile value={false} hidden={false} title="too long" />,
-        ]
+  render() {
+    const visibleItems = [
+      <Tile value={true} hidden={false} title="Test" className="large" />,
+      <Tile value={true} hidden={false} title="ABC" />,
+      <Tile value={false} hidden={false} title="Ehi" className="tall" />,
+      <Tile value={false} hidden={false} title="Dumb" className="wide" />,
+      <Tile value={true} hidden={false} title="Clone" className="tall" />,
+      <Tile value={false} hidden={false} title="Tall" className="tall" />,
+      <Tile value={true} hidden={false} title="Wide" className="wide" />,
+      <Tile value={true} hidden={false} title="Small" />,
+      <Tile value={false} hidden={false} title="Still readin?" />,
+      <Tile value={true} hidden={false} title="Man!!" />,
+      <Tile value={false} hidden={false} title="Stop" />,
+      <Tile value={false} hidden={false} title="This is" />,
+      <Tile value={false} hidden={false} title="too long" />
+    ];
 
-        const hiddenItems = [
-            <Tile value={false} hidden={true} title="As you" />,
-            <Tile value={true} hidden={true} title="can see" />,
-            <Tile value={true} hidden={true} title="this values" />,
-            <Tile value={false} hidden={true} title="are hidden" />,
-        ]
+    const hiddenItems = [
+      <Tile value={false} hidden={true} title="As you" />,
+      <Tile value={true} hidden={true} title="can see" />,
+      <Tile value={true} hidden={true} title="this values" />,
+      <Tile value={false} hidden={true} title="are hidden" />
+    ];
 
-        return (
-            <MuiThemeProvider>
-                <div className="main">
-                    <div className="sidebarHeader" />
-                    <div className="sidebar">
-                        <Sidebar />
-                    </div>
-                    <div className="mainBodyHeader" />
-                    <div className="mainBody">
-                        <div className="itemsList">{visibleItems}</div>
-                        <FlatButton
-                            onClick={() =>
-                                this.setState(oldState => ({
-                                    showHidden: !oldState.showHidden,
-                                }))
-                            }
-                            label={
-                                this.state.showHidden
-                                    ? "Show less"
-                                    : "Show more"
-                            }
-                            fullWidth={true}
-                            className="divider"
-                        />
-                        <div className="itemsList hiddenItems">
-                            {this.state.showHidden ? hiddenItems : ""}
-                        </div>
-                    </div>
-                </div>
-            </MuiThemeProvider>
-        )
-    }
+    return (
+      <MuiThemeProvider>
+        <div className="main">
+          <div className="sidebarHeader">
+            <SidebarHeader />
+          </div>
+          <div className="sidebar">
+            <Sidebar />
+          </div>
+          <div className="mainBodyHeader" />
+          <div className="mainBody">
+            <div className="itemsList">{visibleItems}</div>
+            <FlatButton
+              onClick={() =>
+                this.setState(oldState => ({
+                  showHidden: !oldState.showHidden
+                }))
+              }
+              label={this.state.showHidden ? "Show less" : "Show more"}
+              fullWidth={true}
+              className="divider"
+            />
+            <div className="itemsList hiddenItems">
+              {this.state.showHidden ? hiddenItems : ""}
+            </div>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-export default Main
+export default Main;
--- src/SidebarHeader.js
+++ src/SidebarHeader.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class SidebarHeader extends Component {
   render() {
     return (
-      <img src="/assets/logo.png" width="60px" height="60px" className="logo" />
+      <img src="/assets/logo.svg" width="60px" height="60px" className="logo" />
     );
   }
 }
