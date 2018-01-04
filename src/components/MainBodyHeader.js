f84a4345ece15ca34d61d692f32182e6128b9f3c
Zanin Andrea <99.zanin@gmail.com>
2018-01-04 16:55:30 +0100

    fixed headers

--- src/components/MainBodyHeader.js
+++ src/components/MainBodyHeader.js
@@ -3,7 +3,6 @@ import { graphql } from "react-apollo"
 import gql from "graphql-tag"
 import ActionGrade from "material-ui/svg-icons/action/grade"
 import ActionHistory from "material-ui/svg-icons/action/history"
-import AppBar from "material-ui/AppBar/AppBar"
 
 class MainBodyHeader extends Component {
   render() {
@@ -18,28 +17,17 @@ class MainBodyHeader extends Component {
     }
 
     return (
-      <AppBar
-        className="sidebarHeader"
-        zDepth={1}
-        rounded={false}
-        showMenuIconButton={false}
-      >
-        <div className="mainBodyHeader">
-          {device.icon ? (
-            <img
-              className="deviceIconBig"
-              src={device.icon}
-              alt="device logo"
-            />
-          ) : (
-            <ActionGrade />
-          )}
-          {device.customName}
-          <div className="rightSide">
-            <ActionHistory />
-          </div>
+      <div className="mainBodyHeader">
+        {device.icon ? (
+          <img className="deviceIconBig" src={device.icon} alt="device logo" />
+        ) : (
+          <ActionGrade />
+        )}
+        {device.customName}
+        <div className="rightSide">
+          <ActionHistory />
         </div>
-      </AppBar>
+      </div>
     )
   }
 }
--- src/components/SidebarHeader.js
+++ src/components/SidebarHeader.js
@@ -3,43 +3,35 @@ import SettingsIcon from "material-ui/svg-icons/action/settings"
 import LogOutIcon from "material-ui/svg-icons/action/exit-to-app"
 import IconButton from "material-ui/IconButton"
 import NotificationIcon from "material-ui/svg-icons/social/notifications"
-import AppBar from "material-ui/AppBar"
 
 class SidebarHeader extends Component {
   render() {
     return (
-      <AppBar
-        className="sidebarHeader"
-        zDepth={2}
-        rounded={false}
-        showMenuIconButton={false}
-      >
-        <div className="sidebarHeaderBackground">
-          <img
-            alt="igloo logo"
-            src="/assets/logo.svg"
-            width="60px"
-            height="60px"
-            className="logo notSelectable"
-          />
-          <div className="rightSide">
-            <NotificationIcon className="icon" />
-            <SettingsIcon className="icon" />
-            <IconButton
-              onClick={this.props.logOut}
-              style={{
-                padding: "0",
-                margin: "0 5px 0 5px",
-                width: "24px",
-                height: "24px",
-              }}
-              className="logoutButton"
-            >
-              <LogOutIcon />
-            </IconButton>
-          </div>
+      <div className="sidebarHeader">
+        <img
+          alt="igloo logo"
+          src="/assets/logo.svg"
+          width="60px"
+          height="60px"
+          className="logo notSelectable"
+        />
+        <div className="rightSide">
+          <NotificationIcon className="icon" />
+          <SettingsIcon className="icon" />
+          <IconButton
+            onClick={this.props.logOut}
+            style={{
+              padding: "0",
+              margin: "0 5px 0 5px",
+              width: "24px",
+              height: "24px",
+            }}
+            className="logoutButton"
+          >
+            <LogOutIcon />
+          </IconButton>
         </div>
-      </AppBar>
+      </div>
     )
   }
 }
