b1eb4bd1d1dccaaa29b3cd6ac3d8d2871491e176
Zanin Andrea <99.zanin@gmail.com>
2018-01-05 11:10:49 +0100

    bear error message now synchronized with network changes

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,7 @@ import SidebarHeader from "./components/SidebarHeader"
 import MainBody from "./components/MainBody"
 import MainBodyHeader from "./components/MainBodyHeader"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import { Offline, Online } from "react-detect-offline"
 import "./styles/App.css"
 import "./styles/Tiles.css"
 
@@ -34,14 +35,53 @@ class Main extends Component {
                   deviceId={this.state.selectedDevice}
                   key="mainBodyHeader"
                 />,
-                <MainBody
-                  deviceId={this.state.selectedDevice}
-                  key="mainBody"
-                />,
+                <Online key="mainBody">
+                  <MainBody deviceId={this.state.selectedDevice} />
+                </Online>,
+                <Offline key="offlineMainBody">
+                  <div className="offlineBody mainBody">
+                    <font size="6">
+                      You are not connected, try again in a while
+                    </font>
+                    <br />
+                    <font size="5">
+                      In the meantime why don't you have a nap?
+                    </font>
+                    <br />
+                    <img
+                      alt="Sleeping Polar Bear"
+                      src="/assets/polarBear.svg"
+                      width="400"
+                      height="400"
+                      className="logo notSelectable"
+                    />
+                  </div>
+                </Offline>,
               ]
             : [
                 <div className="mainBodyHeader" key="mainBodyHeader" />,
-                <div className="mainBody" key="mainBody" />,
+                <Online key="mainBody">
+                  <div className="mainBody" />
+                </Online>,
+                <Offline key="offlineMainBody">
+                  <div className="offlineBody mainBody">
+                    <font size="6">
+                      You are not connected, try again in a while
+                    </font>
+                    <br />
+                    <font size="5">
+                      In the meantime why don't you have a nap?
+                    </font>
+                    <br />
+                    <img
+                      alt="Sleeping Polar Bear"
+                      src="/assets/polarBear.svg"
+                      width="400"
+                      height="400"
+                      className="logo notSelectable"
+                    />
+                  </div>
+                </Offline>,
               ]}
         </div>
       </MuiThemeProvider>
