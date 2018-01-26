9e755ebf5a3446d0ea1f7231adac0213e3c8a1a7
Samuele Dassatti <samueledassatti99@gmail.com>
2018-01-26 17:16:00 +0100

    Added a "rearrange tiles" button

--- src/components/MainBodyHeader.js
+++ src/components/MainBodyHeader.js
@@ -2,15 +2,25 @@ import React, { Component } from "react"
 import { graphql } from "react-apollo"
 import gql from "graphql-tag"
 import IconButton from "material-ui/IconButton"
+import Dialog from "material-ui/Dialog"
+import FlatButton from "material-ui/FlatButton"
 
 class MainBodyHeader extends Component {
-  state = { open: false }
+  state = {
+    open: false,
+  }
 
-  openDrawer = () => this.setState({ open: true })
+  handleOpen = () => {
+    this.setState({ open: true })
+  }
 
-  handleClose = () => this.setState({ open: false })
+  handleClose = () => {
+    this.setState({ open: false })
+  }
 
   render() {
+    const actions = [<FlatButton label="Close" onClick={this.handleClose} />]
+
     const { loading, error, device } = this.props.data
     if (loading) {
       return <div className="mainBodyHeader" />
@@ -29,6 +39,23 @@ class MainBodyHeader extends Component {
           <i class="deviceIconBig material-icons">lightbulb_outline</i>
         )}
         <p className="title">{device.customName}</p>
+        <IconButton
+          onClick={this.handleOpen}
+          className="mainBodyHeaderIcon"
+          style={{ marginTop: "6px" }}
+        >
+          <i class="material-icons">mode_edit</i>
+        </IconButton>
+        <Dialog
+          title="Rearrange tiles"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+          className="notSelectable "
+        >
+          [FILL WITH DRAG 'N' DROPPABLE LIST]
+        </Dialog>
       </div>
     )
   }
