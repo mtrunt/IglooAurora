9068ee3ba8b2175da0e7b9c506854c9eaade4587
Samuele Dassatti <samueledassatti99@gmail.com>
2018-02-23 18:12:43 +0100

    Added the option to change user name

--- src/components/settings/SettingsDialog.js
+++ src/components/settings/SettingsDialog.js
@@ -326,6 +326,9 @@ class SettingsDialog extends React.Component {
               <List>
                 <Subheader>Authentication</Subheader>
                 <ListItem
+                  primaryText="Change user name"
+                />
+                <ListItem
                   primaryText="Manage emails"
                   secondaryText="Add or delete emails you use to log in"
                   onClick={this.handleEmailDialogOpen}
