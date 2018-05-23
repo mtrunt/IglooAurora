470ec6a5b821f85d85b815b1d469932b8c631678
Samuele Dassatti <samueledassatti99@gmail.com>
2018-05-23 16:43:58 +0200

    Updates

--- webPushSw.js
+++ webPushSw.js
@@ -3,9 +3,10 @@ self.addEventListener("push", function(event) {
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`)
 
   const pushData = JSON.parse(event.data.text())
-  const title = pushData.content
+  const title = pushData.device.customName
   const options = {
     body: pushData.content,
+    timestamp: pushData.date,
   }
 
   const notification = self.registration.showNotification(title, options)
